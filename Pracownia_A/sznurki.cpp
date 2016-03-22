0051282b3e8347f73d669de53f3a3303866aeee2
Dawid Kostyszak <d.kostyszak@gmail.com>
2016-03-22 23:25:59 +0100

    Pracownia 2 poprawka #2, jest błąd dla testu 0, w przypadku gdy root nie ma lewego syna

--- Pracownia_A/sznurki.cpp
+++ Pracownia_A/sznurki.cpp
@@ -18,13 +18,21 @@ public:
             return insert(node, root);
         } else {
             root = node;
-            counter = 1;
             return node;
         }
     };
 
     void solve() {
-        solve(root);
+        Node *node = find_min(root);
+        while (node != NULL) {
+            solveNode(node);
+            if (root != NULL) {
+                node = find_min(root);
+            } else {
+                node = NULL;
+            }
+        }
+        printf("%llu\n", counter);
     };
 
     Node *root;
@@ -37,8 +45,7 @@ private:
                 return insert(node, leaf->left);
             } else {
                 leaf->left = node;
-                node->parent = leaf->left;
-                counter++;
+                node->parent = leaf;
                 return node;
             }
         } else if(node->value > leaf->value) {
@@ -46,30 +53,14 @@ private:
                 return insert(node, leaf->right);
             } else {
                 leaf->right = node;
-                node->parent = leaf->right;
-                counter++;
+                node->parent = leaf;
                 return node;
             }
         } else {
-            if (leaf->amount == 0) {
-                counter++;
-            }
             leaf->amount += node->amount;
             return leaf;
         }
     };
-
-    void solve(Node *node) {
-        if (node->left != NULL) {
-            solve(node->left);
-        }
-        if (node->amount > 1) {
-            solveNode(node);
-        }
-        if (node->right != NULL) {
-            solve(node->right);
-        }
-    }
 
     void solveNode(Node *node) {
         Node *tmp_node = new Node();
@@ -77,75 +68,77 @@ private:
         tmp_node->amount = node->amount / 2;
 
         if (node->amount % 2 == 0) {
-            node->amount = 0;
-//            delete_node(node);
-            counter--;
+            delete_node(node);
         } else {
-            node->amount = 1;
+            counter++;
+            delete_node(node);
         }
 
         node = insert(tmp_node);
 
         if (node->amount > 1) {
             solveNode(node);
+        } else {
+            counter++;
+            delete_node(node);
         }
     }
 
-//    Node *find_min(Node *node) {
-//        while (node->left != NULL) {
-//            node = node->left;
-//        }
-//        return node;
-//    };
-//
-//    Node *find_successor(Node *node) {
-//        if (node->right != NULL) {
-//            return find_min(node->right);
-//        }
-//
-//        Node *tmp_node = node->parent;
-//        while (tmp_node != NULL && node == tmp_node->right) {
-//            node = tmp_node;
-//            tmp_node = tmp_node->parent;
-//        }
-//        return tmp_node;
-//    };
-//
-//    void delete_node(Node *leaf) {
-//        Node *tmp_node, *node;
-//        if (leaf->left == NULL || leaf->right == NULL) {
-//            node = leaf;
-//        } else {
-//            node = find_successor(leaf);
-//        }
-//
-//        if (node->left != NULL) {
-//            tmp_node = node->left;
-//        } else {
-//            tmp_node = node->right;
-//        }
-//
-//        if (tmp_node != NULL) {
-//            tmp_node->parent = node->parent;
-//        }
-//
-//        if (node->parent == NULL) {
-//            root = tmp_node;
-//        } else if (node == node->parent->left) {
-//            node->parent->left = tmp_node;
-//        } else {
-//            node->parent->right = tmp_node;
-//        }
-//
-//        if (node != leaf) {
-//            leaf->amount = node->amount;
-//            leaf->value = node->value;
-//        }
-//    };
+    Node *find_min(Node *node) {
+        while (node->left != NULL) {
+            node = node->left;
+        }
+        return node;
+    };
+
+    Node *find_successor(Node *node) {
+        if (node->right != NULL) {
+            return find_min(node->right);
+        }
+
+        Node *tmp_node = node->parent;
+        while (tmp_node != NULL && node == tmp_node->right) {
+            node = tmp_node;
+            tmp_node = tmp_node->parent;
+        }
+        return tmp_node;
+    };
+
+    void delete_node(Node *leaf) {
+        Node *tmp_node, *node;
+        if (leaf->left == NULL || leaf->right == NULL) {
+            node = leaf;
+        } else {
+            node = find_successor(leaf);
+        }
+
+        if (node->left != NULL) {
+            tmp_node = node->left;
+        } else {
+            tmp_node = node->right;
+        }
+
+        if (tmp_node != NULL) {
+            tmp_node->parent = node->parent;
+        }
+
+        if (node->parent == NULL) {
+            root = tmp_node;
+        } else if (node == node->parent->left) {
+            node->parent->left = tmp_node;
+        } else {
+            node->parent->right = tmp_node;
+        }
+
+        if (node != leaf) {
+            leaf->amount = node->amount;
+            leaf->value = node->value;
+        }
+        delete node;
+    };
 };
 
 int main() {
-    clock_t tStart = clock();
     unsigned long long n, value, amount;
     Tree tree;
 
@@ -159,8 +152,5 @@ int main() {
     }
 
     tree.solve();
-
-    printf("%llu\n", tree.counter);
-    printf("Time taken: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
     return 0;
 }
